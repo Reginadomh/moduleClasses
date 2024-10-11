638884dac2061d2d6da12e8affdea104d00cdad6
Reginadomh <domherregi@gmail.com>
2024-10-10 22:21:21 -0600

    Add files via upload

---
+++ moduleClasses/claseAlumno.py
@@ -0,0 +1,42 @@
+class claseAlumno:
+    def __init__(self):
+        self.__nombre = None
+        self.__apellido_paterno = None
+        self.__apellido_materno = None
+        self.__no_control = None
+        self.__semestre = None
+
+    # Métodos para establecer valores (setters)
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def set_apellido_paterno(self, apellido_paterno):
+        self.__apellido_paterno = apellido_paterno
+
+    def set_apellido_materno(self, apellido_materno):
+        self.__apellido_materno = apellido_materno
+
+    def set_no_control(self, no_control):
+        self.__no_control = no_control
+
+    def set_semestre(self, semestre):
+        self.__semestre = semestre
+
+    # Métodos para obtener valores (getters)
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_apellido_paterno(self):
+        return self.__apellido_paterno
+
+    def get_apellido_materno(self):
+        return self.__apellido_materno
+
+    def get_no_control(self):
+        return self.__no_control
+
+    def get_semestre(self):
+        return self.__semestre
+
+    def get_Nombre_Completo(self):
+        return f"{self.__nombre} {self.__apellido_paterno} {self.__apellido_materno}"
